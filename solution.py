14c5fabf9842bd65f3ffe302d4964be60d4f78fd
Alyssa <alyssahuang@college.harvard.edu>
2022-09-15 02:23:25 -0400

    Fixed style according to flake8 linter

--- solution.py
+++ solution.py
@@ -1,16 +1,18 @@
+import math
+
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-import math
 
 def mergeKLists(lists):
     head = None
     current = None
     for i in reversed(range(len(lists))):
-        if lists[i] == None:
+        if lists[i] is None:
             lists.pop(i)
     while len(lists) > 0:
         min = (math.inf, None)
@@ -18,15 +20,16 @@ def mergeKLists(lists):
             if list.val < min[0]:
                 min = (list.val, i)
         lists[min[1]] = lists[min[1]].next
-        if lists[min[1]] == None:
+        if lists[min[1]] is None:
             lists.pop(min[1])
-        if head == None:
+        if head is None:
             head = ListNode(min[0])
             current = head
         else:
             current.next = ListNode(min[0])
             current = current.next
     return head
+
 
 def createLinkedLists(lists):
     res = []
@@ -39,6 +42,7 @@ def createLinkedLists(lists):
         res.append(head)
     return res
 
-lists = createLinkedLists([[1,4,5],[1,3,4],[2,6]])
+
+lists = createLinkedLists([[1, 4, 5], [1, 3, 4], [2, 6]])
 # lists = createLinkedLists([[]])
 result = mergeKLists(lists)
